3cb6d1795ae7d51f930945f5f5c9422a37afc8e7
basswoman <54603761+basswoman@users.noreply.github.com>
2024-07-10 16:11:10 +0300

    rubric

--- frontend/src/pages/Rubric.jsx
+++ frontend/src/pages/Rubric.jsx
@@ -4,7 +4,7 @@ import ViewGradingRubricDetails from '../components/ViewGradingRubricDetails';
 const Rubric = ({ selected_rubric_id, setRubricID }) => {
   return (
     <div className="container">
-      <p className="center">View or change your Grading Rubric</p>
+      {/* <p className="center">View or change your Grading Rubric</p> */}
 
       <ViewGradingRubricDetails
         rubric_id={selected_rubric_id}
