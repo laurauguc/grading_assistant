3b31528c0b656ee96133fe07a53184defc25bef5
basswoman <54603761+basswoman@users.noreply.github.com>
2024-07-10 15:52:55 +0300

    flexbox

--- frontend/src/components/ObtainRubricNames.js
+++ frontend/src/components/ObtainRubricNames.js
@@ -11,14 +11,13 @@ function ObtainRubricNames({ selected_rubric_id, setRubricID }) {
 
   var REACT_APP_HOST_BASE_URL;
   if (process.env.NODE_ENV === 'development') {
-    REACT_APP_HOST_BASE_URL="http://localhost:8000/"
+    REACT_APP_HOST_BASE_URL = 'http://localhost:8000/';
   } else {
-    REACT_APP_HOST_BASE_URL="http://laurauguc.pythonanywhere.com/"
+    REACT_APP_HOST_BASE_URL = 'http://laurauguc.pythonanywhere.com/';
   }
 
   useEffect(() => {
-    fetch(
-      REACT_APP_HOST_BASE_URL.concat("api/obtain-rubric-names/")) // 'http://localhost:8000/api/obtain-rubric-names/') // alternative: {params: {student_assignment: props.student_assignment, grading_rubric: props.grading_rubric}}
+    fetch(REACT_APP_HOST_BASE_URL.concat('api/obtain-rubric-names/')) // 'http://localhost:8000/api/obtain-rubric-names/') // alternative: {params: {student_assignment: props.student_assignment, grading_rubric: props.grading_rubric}}
       .then(rubric_response => rubric_response.json())
       .then(setRubrics)
       .catch(error => {
