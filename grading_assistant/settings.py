c39ec94b0e196b6528eb1a04470d0338411977b8
laurauguc <l.uguccioni1@gmail.com>
2024-07-13 18:04:58 -0700

    static folder renaming

--- grading_assistant/settings.py
+++ grading_assistant/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = "static"
+STATIC_ROOT = "build"
 
 STATIC_URL = 'static/'
 
