7143b6efbf15944c4c79f18fc13f857c116473dc
basswoman <54603761+basswoman@users.noreply.github.com>
2024-07-18 20:19:52 +0300

    custom rubric

--- backend/views.py
+++ backend/views.py
@@ -14,6 +14,7 @@ from pathlib import Path
 #import os
 from django.conf import settings
 from . import utils
+import tempfile
 
 load_dotenv(Path(".env"))
 GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
@@ -60,11 +61,33 @@ def obtain_rubric(request):
     del rubric['_state'] # need to remove this created key in order for object to be serializable
     return Response(rubric)
 
-@api_view(['GET'])
+@api_view(['POST'])
 def convert_docx_to_md(request):
-    docx_path = request.query_params["docx_path"]
-    # delete commented line (temporarily here to provide example for testing)
-    #docx_path = "/Users/laura/Desktop/grading_assistant/external_data/rubric_markdown_test/us_history_rubric_test.docx"
-    converter = utils.DocxToMarkdownConverter(docx_path)
-    md_content = converter.convert()
+    # docx_path = request.query_params["docx_path"]
+    # # delete commented line (temporarily here to provide example for testing)
+    # #docx_path = "/Users/laura/Desktop/grading_assistant/external_data/rubric_markdown_test/us_history_rubric_test.docx"
+    # converter = utils.DocxToMarkdownConverter(docx_path)
+    # md_content = converter.convert()
+    # return Response(md_content)
+
+    if 'file' not in request.FILES:
+        return Response({"error": "No file provided"}, status=400)
+    
+    uploaded_file = request.FILES['file']
+    
+    # Create a temporary file in /tmp
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.docx') as tmp_file:
+        for chunk in uploaded_file.chunks():
+            tmp_file.write(chunk)
+        tmp_file_path = tmp_file.name
+
+    try:
+        # Pass the temporary file path to the converter
+        converter = utils.DocxToMarkdownConverter(tmp_file_path)
+        md_content = converter.convert()
+    finally:
+        # Delete the temporary file
+        os.remove(tmp_file_path)
+    
     return Response(md_content)
+
